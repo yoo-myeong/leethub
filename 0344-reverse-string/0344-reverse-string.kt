c7f05a598fe325c6b0c11b470a74acb25106bdbf
purpleMocha <60503633+yoo-myeong@users.noreply.github.com>
2024-04-15 18:28:22 +0900

    Time: 231 ms (62.26%), Space: 41.4 MB (100%) - LeetHub

---
+++ 0344-reverse-string/0344-reverse-string.kt
@@ -0,0 +1,12 @@
+class Solution {
+    fun reverseString(s: CharArray): Unit {
+        var start = 0
+        var end = s.size - 1
+
+        while(start < end) {
+            s[start] = s[end].also { s[end] = s[start] }
+            start ++
+            end --
+        }
+    }
+}
